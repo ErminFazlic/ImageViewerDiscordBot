08a6b17735e0f24f8144c9e805cbf5dbfc1d9652
ErminFazlic <Erminfazlic97@gmail.com>
2021-04-01 18:20:35 +0000

    Added functionality so when !img, followed by a message, command is called the bot returns an image of the message along with the image URL.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import discord
 import os
+from google_images_search import GoogleImagesSearch
+from io import BytesIO
+from PIL import Image
+
 client =discord.Client()
+gis= GoogleImagesSearch(os.getenv('api_key'), os.getenv('search_engine'))
+
 
 @client.event
 async def on_ready():
@@ -11,10 +17,22 @@ async def on_message(message):
   if message.author ==client.user:
     return
  
+  if message.content.startswith('!img'):
+    string=message.content
+    query=string[4:]
 
-  
-  if message.content.startswith('$play'):
-    await message.channel.send('!play toto africa')
+    gis.search({'q': query})
+    bytes=BytesIO()
+
+    for image in gis.results():
+      bytes.seek(0)
+      raw_image_data=image.get_raw_data()
+      image.copy_to(bytes, raw_image_data)
+      bytes.seek(0)
+      tmp_img=Image.open(bytes)
+      tmp_img.save('my_image.jpg')
+      file=discord.File('my_image.jpg')
+      await message.channel.send('"'+image.url+'"', file=file)
  
 
 client.run(os.getenv('Token'))
